4e1c86f6d60c79b019f0ed059c7fa17ff651b437
anjali <singhanjali1230@gmail.com>
2026-09-10 01:26:39 -0700

    add  fix  404

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { GoogleGenAI } = require("@google/genai");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const upload = multer({
   storage: multer.memoryStorage(),
